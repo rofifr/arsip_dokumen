ca62f4d86f1bf3184f7fe865c6f918fc0caaec47
aqluzd <dzulqafauz@gmail.com>
2024-07-14 17:39:51 +0700

    typo

--- petugas/arsip.php
+++ petugas/arsip.php
@@ -46,7 +46,7 @@
                 if(isset($_GET['alert'])){
                     if($_GET['alert'] == "gagal"){
                         ?>
-                        <div class="alert alert-danger">File arsip gagal diupload. krena demi keamanan file .php tidak diperbolehkan.</div>
+                        <div class="alert alert-danger">File arsip gagal diupload. karena demi keamanan file .php tidak diperbolehkan.</div>
                         <?php
                     }else{
                         ?>
